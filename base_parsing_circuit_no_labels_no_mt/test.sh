aa232acb4b3bb8f409cef3870ad7faae25c19a0f
Harjasleen Malvai <harjasleen_malvai@brown.edu>
2024-07-31 05:27:03 -0700

    Added structs

---
+++ base_parsing_circuit_no_labels_no_mt/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eux
+nargo execute base_parsing_circuit_no_labels_no_mt --package base_parsing_circuit_no_labels_no_mt
+
+BACKEND=../bb
+
+$BACKEND write_vk -b ../target/base_parsing_circuit_no_labels_no_mt.json -o ../target/vk   
+mkdir -p proofs
+start=$(python3 -c "import time; print(time.time_ns())")
+$BACKEND prove -b ../target/base_parsing_circuit_no_labels_no_mt.json -w ../target/base_parsing_circuit_no_labels_no_mt.gz -o ./proofs/proof
+end=$(python3 -c "import time; print(time.time_ns())")
+elapsed=$(echo "scale=9; ($end - $start) / 1000000000" | bc)
+echo "Proving took $elapsed s" 
+# mkdir -p target
+# cp ../target/vk ./target/vk
+# $BACKEND verify -p ./proofs/proof -v ./target/vk
+
+# FULL_PROOF_AS_FIELDS="$($BACKEND proof_as_fields -p ./proofs/proof -k ./target/vk -o -)"
+# PUBLIC_INPUTS=$(echo $FULL_PROOF_AS_FIELDS | jq -r '.[:18]')                            
+# PROOF_AS_FIELDS=$(echo $FULL_PROOF_AS_FIELDS | jq -r '.[18:]')
+
+# $BACKEND vk_as_fields -k ./target/vk -o ./target/vk_as_fields
+# VK_HASH=$(jq -r '.[0]' ./target/vk_as_fields)
+# VK_AS_FIELDS=$(jq -r '.[1:]' ./target/vk_as_fields)
+
+# echo "proof=$PROOF_AS_FIELDS" >> ../recursive_parsing_circuit/Prover.toml
+# echo "verification_key=$VK_AS_FIELDS" >> ../recursive_parsing_circuit/Prover.toml
+# echo "key_hash=\"$VK_HASH\"" >> ../recursive_parsing_circuit/Prover.toml
+# cat ../recursive_parsing_circuit/prods >> ../recursive_parsing_circuit/Prover.toml
